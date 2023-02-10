24f616bdda06f75d7d0f72d0503d8f46bf9e34a3
dn004 <d.ntwari@alustudent.com>
2023-02-10 13:59:02 +0000

    lilvy

---
+++ minimum_operations/0-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#Testing
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+
+print(.format(n, minOperations(n)))
+
+n = 12
+
+print(.format(n, minOperations(n)))
